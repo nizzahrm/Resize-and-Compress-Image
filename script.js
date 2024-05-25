eb318cea233f6d24cece33c92e8b1fdee1273cc2
nizzahrm <nurulizzahromadhani@gmail.com>
2024-05-25 17:15:00 +0700

    perbaiki js

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const uploadBox = document.querySelector(".upload-box"),
-previewImg = uploadBox.querySelector("img"),
-fileInput = uploadBox.querySelector("input");
+fileInput = uploadBox.querySelector("input[type='file']"),
+previewImg = uploadBox.querySelector("img");
 
 const loadFile = (e) => {
-    const file = e.target.files[0]; //getting first user selected file
+    const file = e.target.files[0]; // getting first user selected file
     if(!file) return; // return if user hasn't selected any file
-    previewImg.src = URL.createObjectURL(file);//passing selected file url to preview img src
-    previewImg.addEventListener("load",()=> {
+    previewImg.src = URL.createObjectURL(file); // passing selected file url to preview img src
+    previewImg.style.display = "block"; // display the preview image
+    previewImg.addEventListener("load", () => {
         document.querySelector(".wrapper").classList.add("active");
-    })
+    });
     console.log(file);
 }
 
 fileInput.addEventListener("change", loadFile);
-uploadBox.addEventListener("click",()=> fileInput.click());
+uploadBox.addEventListener("click", () => fileInput.click());
