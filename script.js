1c85527728fe58354e431a875bce94dfc072fcf3
nizzahrm <nurulizzahromadhani@gmail.com>
2024-05-25 21:31:34 +0700

    height progress

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const uploadBox = document.querySelector(".upload-box"),
 previewImg = uploadBox.querySelector("#preview-img"),
-fileInput = uploadBox.querySelector("#file-input");
+fileInput = uploadBox.querySelector("#file-input"),
+widthInput = document.querySelector(".width input"),
+heightInput = document.querySelector(".height input"),
+ratioInput = document.querySelector(".ratio input");
+
+let ogImageRatio;
 
 const loadFile = (e) => {
     const file = e.target.files[0]; // getting first user selected file
@@ -8,10 +13,25 @@ const loadFile = (e) => {
     previewImg.src = URL.createObjectURL(file); // passing selected file url to preview img src
     previewImg.style.display = "block"; // display the preview image
     previewImg.addEventListener("load", () => {
+        widthInput.value = previewImg.naturalWidth;
+        heightInput.value = previewImg.naturalHeight;
+        ogImageRatio = previewImg.naturalWidth / previewImg.naturalHeight;
+
         document.querySelector(".wrapper").classList.add("active");
     });
-    console.log(file);
+    
 }
+
+widthInput.addEventListener("keyup", ()=>{
+    //getting height according to the ratio checkbos status
+    const height = ratioInput.checked ? widthInput.value / ogImageRatio : heightInput.value;
+    heightInput.value = Math.floor(height);
+
+});
+
+
+
+
 
 fileInput.addEventListener("change", loadFile);
 uploadBox.addEventListener("click", () => fileInput.click());
